a0c65ec3a9d16770f42bfeae1c21cc99e9ca0b45
mouse-rider <kishorekumarips28@gmail.com>
2024-12-01 11:19:40 +0530

    hello

---
+++ kishore_music_app/js/playlist.js
@@ -0,0 +1,40 @@
+let songs = [
+
+  {
+    name: "Kelaamal Kaiyile",
+    artist: "Sriram Parthasarathy & Saindhavi",
+    img: "kelaamal_kaiyile",
+    audio: "kelaamal_kaiyile"
+  },
+  {
+    name: "Enathuyire Enathuyire",
+    artist: "Harris Jayaraj",
+    img: "enathuyire",
+    audio: "enathuyire"
+  },
+  {
+    name: "Anbe Anbe Kollathe",
+    artist: "HariHaran",
+    img: "anbe_kollathe_hariharan",
+    audio: "anbe_kollathe_hariharan"
+  },
+  {
+    name: "Kanaa Kangiren",
+    artist: "HariHaran",
+    img: "kanaa_kangiren",
+    audio: "kanaa_kangiren"
+  },
+
+  {
+    name: "Kannodu Kanbathellam",
+    artist: " A.R. Rahman, Nithyasree Mahadevan",
+    img: "kannodu_kanbathellam",
+    audio: "kannodu_kanbathellam"
+  },
+  {
+    name: "Kathal Rojave",
+    artist: "S. P. Balasubrahmanyam",
+    img: "spb",
+    audio: "kathal_rojave"
+  }
+]
